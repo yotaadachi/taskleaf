4eb1983a1f83f12b9fe304398f30bbbf219c6042
vagrant <vagrant@localhost.localdomain>
2020-10-12 06:15:10 +0000

    update

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,6 +4,7 @@ class TasksController < ApplicationController
   end
 
   def show
+    @task = Task.find(params[:id])
   end
 
   def new
@@ -13,10 +14,17 @@ class TasksController < ApplicationController
   def create
     task = Task.new(task_params)
     task.save!
-    redirect_to tasks_url, notice:"タスク#{task.name}を登録しました"
+    redirect_to tasks_url, notice:"タスク『#{task.name}』を登録しました"
   end
 
   def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    task = Task.find(params[:id])
+    task.update!(task_params)
+    redirect_to tasks_url, notice:"タスク『#{task.name}』を更新しました"
   end
 
   private
