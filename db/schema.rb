369bbf7af125e2ddde243876c9edc04285b582b2
vagrant <vagrant@localhost.localdomain>
2020-10-13 03:27:52 +0000

    update model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_12_044211) do
+ActiveRecord::Schema.define(version: 2020_10_13_021936) do
 
   create_table "tasks", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
